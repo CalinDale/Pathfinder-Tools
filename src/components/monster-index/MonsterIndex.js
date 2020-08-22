5d23b566dd283be012fcef7613e57664b67045c0
Calin Dale <citdale@gmail.com>
2020-08-22 21:38:11 +1200

    More reorganising

--- src/components/monster-index/MonsterIndex.js
+++ src/components/monster-index/MonsterIndex.js
@@ -1,15 +1,90 @@
-import React from "react";
+import React, { Component } from "react";
 import MonsterIndexList from "./MonsterIndexList";
 import MonsterIndexFilters from "./filters/MonsterIndexFilters";
+import { connect } from "react-redux";
+import { updateFilteredMonsters } from "../../store/actions/filteredMonstersActions";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
-const MonsterIndex = (props) => {
-	return (
-		<div className="MonsterIndex">
-			<h1>Monster Index</h1>
-			<MonsterIndexFilters />
-			<MonsterIndexList />
-		</div>
-	);
+class MonsterIndex extends Component {
+	filterFunctions = {
+		name: (monster, filters) => {
+			console.log("currently filtering " + monster.name + " by name");
+			let monsterName = monster.name.replace(/\s+/g, "").toLowerCase();
+			let filterTerm = filters.name.value.replace(/\s+/g, "").toLowerCase();
+			return monsterName.includes(filterTerm);
+		},
+	};
+
+	onFiltersChange = (filters) => {
+		console.log(filters);
+		this.props.updateFilteredMonsters(
+			this.filterMonsters(this.props.allMonsters, filters)
+		);
+	};
+
+	onFiltersAdd = (filter) => {
+		let filters = {};
+		filters[filter.filterName] = filter;
+
+		this.props.updateFilteredMonsters(
+			this.filterMonsters(this.props.filteredMonsters, filters)
+		);
+	};
+
+	createFilterFunctionsArray(filters) {
+		let functions = [];
+		Object.values(filters).forEach((filter) => {
+			functions.push(this.filterFunctions[filter.filterName]);
+		});
+		return functions;
+	}
+
+	filterMonsters(monsters, filters) {
+		let filterFunctions = this.createFilterFunctionsArray(filters);
+
+		return monsters.filter((monster) => {
+			let result = true;
+			filterFunctions.forEach((filterFunction) => {
+				result = result && filterFunction(monster, filters);
+			});
+			return result;
+		});
+	}
+
+	render() {
+		return (
+			<div className="MonsterIndex">
+				<h1>Monster Index</h1>
+				<MonsterIndexFilters
+					onFiltersChange={this.onFiltersChange}
+					onFiltersAdd={this.onFiltersAdd}
+				/>
+				<MonsterIndexList />
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = (state) => {
+	const allMonsters = state.firestore.ordered.monsters
+		? state.firestore.ordered.monsters
+		: null;
+	return {
+		allMonsters: allMonsters,
+		filteredMonsters: state.monsters.filtered,
+	};
 };
 
-export default MonsterIndex;
+const mapDispatchToProps = (dispatch) => {
+	return {
+		updateFilteredMonsters: (filteredMonsters) => {
+			dispatch(updateFilteredMonsters(filteredMonsters));
+		},
+	};
+};
+
+export default compose(
+	connect(mapStateToProps, mapDispatchToProps),
+	firestoreConnect([{ collection: "monsters" }])
+)(MonsterIndex);
--- src/components/monster-index/filters/MonsterIndexFilters.js
+++ src/components/monster-index/filters/MonsterIndexFilters.js
@@ -1,50 +1,44 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import { updateFilteredMonsters } from "../../../store/actions/filteredMonstersActions";
 import MonsterNameFilter from "./MonsterNameFilter";
-import { firestoreConnect } from "react-redux-firebase";
-import { compose } from "redux";
 
 class MonsterIndexFilters extends Component {
 	state = {
-		filters: { name: "", type: "" },
-	};
-
-	filterFunctions = {
-		name: (monster, filters) => {
-			let monsterName = monster.name.replace(/\s+/g, "").toLowerCase();
-			let filterTerm = filters.name.replace(/\s+/g, "").toLowerCase();
-			return monsterName.includes(filterTerm);
-		},
+		filters: { name: { filterName: "name", value: "", inUse: false } },
+		inUseFilters: {},
 	};
 
 	updateFilter = (name, value) => {
 		let filters = this.state.filters;
-		filters[name] = value;
-		this.setState({ filters });
-		this.props.updateFilteredMonsters(
-			this.filterMonsters(this.props.allMonsters, filters)
-		);
+		filters[name].value = value;
+
+		let oldInUse = this.state.filters[name].inUse;
+		filters[name].inUse = this.filterIsInUse(name, value);
+
+		let inUseFilters = this.state.inUseFilters;
+
+		if (filters[name].inUse === true) {
+			inUseFilters[name] = filters[name];
+		} else if (filters[name].inUse === false) {
+			delete this.state.inUseFilters[name];
+		}
+		this.setState({ filters, inUseFilters });
+
+		if (oldInUse === false && filters[name].inUse === true) {
+			this.props.onFiltersAdd(filters[name]);
+		} else if (oldInUse === false && filters[name.inUse === false]) {
+			//Do nothing
+		} else {
+			this.props.onFiltersChange(inUseFilters);
+		}
 	};
 
-	createFilterFunctionsArray(filters) {
-		let functions = [];
-		if (filters.name !== "") functions.push(this.filterFunctions.name);
-		return functions;
-	}
-
-	filterMonsters(monsters, filters) {
-		//determine which filters are being used.
-		//Do this beforehand so we're not re-checking which filters to use for every monster.
-		let filterFunctions = this.createFilterFunctionsArray(filters);
-
-		return monsters.filter((monster) => {
-			let result = true;
-			filterFunctions.forEach((filterFunction) => {
-				result = result && filterFunction(monster, filters);
-			});
-			return result;
-		});
+	filterIsInUse(name, value) {
+		switch (name) {
+			case "name":
+				return value !== "";
+			default:
+				break;
+		}
 	}
 
 	render() {
@@ -52,7 +46,7 @@ class MonsterIndexFilters extends Component {
 			<form className="MonsterFilters">
 				<h2>Monster Filters</h2>
 				<MonsterNameFilter
-					value={this.state.filters.name}
+					value={this.state.filters.name.value}
 					updateFilter={this.updateFilter}
 				/>
 			</form>
@@ -60,24 +54,4 @@ class MonsterIndexFilters extends Component {
 	}
 }
 
-const mapStateToProps = (state) => {
-	const allMonsters = state.firestore.ordered.monsters
-		? state.firestore.ordered.monsters
-		: null;
-	return {
-		allMonsters: allMonsters,
-	};
-};
-
-const mapDispatchToProps = (dispatch) => {
-	return {
-		updateFilteredMonsters: (filteredMonsters) => {
-			dispatch(updateFilteredMonsters(filteredMonsters));
-		},
-	};
-};
-
-export default compose(
-	connect(mapStateToProps, mapDispatchToProps),
-	firestoreConnect([{ collection: "monsters" }])
-)(MonsterIndexFilters);
+export default MonsterIndexFilters;
